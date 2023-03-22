fcc97374804e2d5222b93b03db79c9193c9a743d
Ülvi Süleymanov <ulvi.suleymanov.yusif@gmail.com>
2023-03-22 16:27:59 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+//        //tek setir ucun komment.
+//        /*
+//        * cox setirli komment*/
+//        int x = 8;// tam ededleri int tipi ile ifade edirik. soldan saga qaydasi type safety verir.
+//        int y = 6; // Riyazi operasiya Arithmatic operations + - * / %
+//        int c = x+y;
+//        int b = y-x;
+//        int d = x*y;
+//        double l = x/(double)y;// burada tam hissesi 0 kesir hissesi ise atilir.
+//        //double kesir ededlerinide ozunde saxlayan butun ededlere double deyilir.
+//        // casting int i double cast etdik cast emek bir seyi basqa seye cevirmek demekdir.
+//        int t = x/y; //bu tam edidi qaytarir kesir ededi delete olunur.
+//        double r = x % y ; // bu bize kesir ededini qaytarir.
+//         = asign olunma operatoru.
+//        int x = 5; // int xsin tipidir x ozu variabledir 5 ise onun valuesudur.
+//        char character = '$';// bir simvolu ifade edir.
+//        System.out.println(character);
+//        boolean isTrue = true; // boolean yalniz iki data saxliyir ya true ya false.
+//        String name = "Ulvi"; // ozunde char[] massivi saxlayan bir object.
+         long x = 5; // long ile int arasindaki ferq onlarin size dir yeni memory slotda tutqu yerdir. long boyuk int kicik
+        int y = (int) x; // manual castinge explicit casting deyilir. bu ona goredirki data itkisi sadece programistin uzerine dususun.
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,5 @@
+public class Test{
+ public static void foo(){
+	System.out.println("Hello");
+}
+}
